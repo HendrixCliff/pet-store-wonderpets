484c1359049eafb609914ac3fcdd895e354de1ab
Igwegbe Godwin Chukwuebuka <godwincliff10@gmail.com>
2024-10-20 11:24:35 +0100

    Done

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -15,7 +15,7 @@ type Props = {
 export default async function Gallery({ topic = "pet", page}: Props) {
 let url 
 
-if (topic === "pet" && !page) {
+if (topic === "pet") {
       url = `https://api.pexels.com/v1/search?query=pet&per_page=30&page=1`
 }
  else if (topic !== "pet" && !page) { 
@@ -24,9 +24,7 @@ if (topic === "pet" && !page) {
  else if (topic !== "pet" && page) {
       url = `https://api.pexels.com/v1/search?query=${topic}&per_page=35&page=${page}`
  }
- else {
- url = `https://api.pexels.com/v1/search?query=pet&per_page=35&page=${page}`
- }
+ 
      
 const images: ImagesResults | undefined = await fetchImages(url)
     
